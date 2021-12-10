ef1b8552d912fb37ae3c6df4c3d4e4378ba421ae
shashank gaihre <sharmashashank2055@gmail.com>
2021-12-10 16:06:40 +0545

    codingchallange completed

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.twoactivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,43 +11,160 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE =
-            "com.example.twoactivities.extra.MESSAGE";
+    public static final String EXTRA_MESSAGE
+            = "com.example.twoactivities.extra.MESSAGE";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final int TEXT_REQUEST = 1;
-
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
+    private TextView textView5;
+    private TextView textView6;
+    private TextView textView7;
+    private TextView textView8;
+    private TextView textView9;
+    private TextView textView10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        textView1 =findViewById(R.id.textView1);
+        textView2 =findViewById(R.id.textView2);
+        textView3 =findViewById(R.id.textView3);
+        textView4 =findViewById(R.id.textView4);
+        textView5 =findViewById(R.id.textView5);
+        textView6 =findViewById(R.id.textView6);
+        textView7 =findViewById(R.id.textView7);
+        textView8 = findViewById(R.id.textView8);
+        textView9 =findViewById(R.id.textView9);
+        textView10 =findViewById(R.id.textView10);
+
+
+        textView1.setText("");
+        textView2.setText("");
+        textView3.setText("");
+        textView4.setText("");
+        textView5.setText("");
+        textView6.setText("");
+        textView7.setText("");
+        textView8.setText("");
+        textView9.setText("");
+        textView10.setText("");
+
+        Intent replyIntent =getIntent();
+
+
+        if (savedInstanceState != null) {
+
+            if (savedInstanceState.getString("text_1")!= null) {
+                textView1.setText(savedInstanceState.getString("text_1"));
+            }if (savedInstanceState.getString("text_2")!= null) {
+                textView2.setText(savedInstanceState.getString("text_2"));
+            }if (savedInstanceState.getString("text_3")!= null) {
+                textView3.setText(savedInstanceState.getString("text_3"));
+            }if (savedInstanceState.getString("text_4")!= null) {
+                textView4.setText(savedInstanceState.getString("text_4"));
+            }if (savedInstanceState.getString("text_5")!= null) {
+                textView5.setText(savedInstanceState.getString("text_5"));
+            }if (savedInstanceState.getString("text_6")!= null) {
+                textView6.setText(savedInstanceState.getString("text_6"));
+            }if (savedInstanceState.getString("text_7")!= null) {
+                textView7.setText(savedInstanceState.getString("text_7"));
+            }if (savedInstanceState.getString("text_8")!= null) {
+                textView8.setText(savedInstanceState.getString("text_8"));
+            }if (savedInstanceState.getString("text_9")!= null) {
+                textView9.setText(savedInstanceState.getString("text_9"));
+            }if (savedInstanceState.getString("text_10")!= null) {
+                textView10.setText(savedInstanceState.getString("text_10"));
+            }
+        }
     }
+
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
     @Override
-    public void onActivityResult(int requestCode,
-                                 int resultCode, Intent data) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (textView1.getText() != "") {
+            outState.putString("text_1",
+                    textView1.getText().toString());
+        }
+        if (textView2.getText() != "") {
+            outState.putString("text_2",
+                    textView2.getText().toString());
+        }
+        if (textView3.getText() != "") {
+            outState.putString("text_3",
+                    textView3.getText().toString());
+        }
+        if (textView4.getText() != "") {
+            outState.putString("text_4",
+                    textView4.getText().toString());
+        }
+        if (textView5.getText() != "") {
+            outState.putString("text_5",
+                    textView5.getText().toString());
+        }
+        if (textView6.getText() != "") {
+            outState.putString("text_6",
+                    textView6.getText().toString());
+        }
+        if (textView7.getText() != "") {
+            outState.putString("text_7",
+                    textView7.getText().toString());
+        }
+        if (textView8.getText() != "") {
+            outState.putString("text_8",
+                    textView8.getText().toString());
+        }
+        if (textView9.getText() != "") {
+            outState.putString("text_9",
+                    textView9.getText().toString());
+        }
+        if (textView10.getText() != "") {
+            outState.putString("text_10",
+                    textView10.getText().toString());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        Log.d(LOG_TAG, "here d!");
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == TEXT_REQUEST) {
             if (resultCode == RESULT_OK) {
                 String reply =
                         data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
+
+
+                if (textView1.getText() == "") {
+                    textView1.setText(reply);
+                } else if (textView2.getText() == "") {
+                    textView2.setText(reply);
+                } else if (textView3.getText() == "") {
+                    textView3.setText(reply);
+                } else if (textView4.getText() == "") {
+                    textView4.setText(reply);
+                } else if (textView5.getText() == "") {
+                    textView5.setText(reply);
+                } else if (textView6.getText() == "") {
+                    textView6.setText(reply);
+                } else if (textView7.getText() == "") {
+                    textView7.setText(reply);
+                } else if (textView8.getText() == "") {
+                    textView8.setText(reply);
+                } else if (textView9.getText() == "") {
+                    textView9.setText(reply);
+                } else if (textView10.getText() == "") {
+                    textView10.setText(reply);
+                }
             }
         }
-    }
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,21 +13,87 @@ public class SecondActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY =
             "com.example.twoactivities.extra.REPLY";
-    private EditText mReply;
+
+    private Button button_apple;
+    private Button button_cheese;
+    private Button button_orange;
+    private Button button_rice;
+    private Button button_icecream;
+    private Button button_egg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+
+        button_apple =findViewById(R.id.button_apple);
+        button_egg =findViewById(R.id.button_egg);
+        button_rice =findViewById(R.id.button_rice);
+        button_orange =findViewById(R.id.button_orange);
+        button_icecream =findViewById(R.id.button_icecream);
+        button_cheese =findViewById(R.id.button_cheese);
+
+
+
     }
 
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
+
+
+    public void btnApple(View view) {
+        String reply = "Apple";
+
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+    public void btnCheese(View view) {
+        String reply = "Cheese";
+
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+    public void btnEgg(View view) {
+        String reply = "Egg";
+
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+    public void btnIcecream(View view) {
+        String reply = "Ice Cream";
+
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+    public void btnOrange(View view) {
+        String reply = "Orange";
+
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+    public void btnRice(View view) {
+        String reply = "Rice";
+
+
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
